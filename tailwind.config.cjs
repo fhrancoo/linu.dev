54d300ddaf190641a405ad536981280fcf97c38c
linuxmobile <bdiez19@gmail.com>
2023-01-15 10:29:24 -0300

    Feat 🦄: Added some files and little changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'mona-sans': ['Mona Sans'],
+				'hubot-sans': ['Hubot Sans']
+			}
+		}
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/line-clamp')
+	]
 }
