8a3eed0d79cfa4ff6e3f0ff1ce95d35dbf633517
linuxmobile <bdiez19@gmail.com>
2023-01-15 18:44:42 -0300

    Refactor 🦄: removed to services

---
+++ src/services/menu.js
@@ -0,0 +1,16 @@
+const MENUTOGGLE = document.getElementById('menutoggle')
+const MENUSECTION = document.getElementById('menuSection')
+const SPAN1 = document.getElementById('span1')
+const SPAN2 = document.getElementById('span2')
+
+export const menuHandler = () => {
+    MENUTOGGLE.addEventListener('click', () => {
+        MENUSECTION.classList.toggle('hidden')
+        MENUSECTION.classList.toggle('flex')
+        SPAN1.classList.toggle('translate-y-1.5')
+        SPAN1.classList.toggle('rotate-45')
+        SPAN2.classList.toggle('w-4')
+        SPAN2.classList.toggle('-translate-y-1.5')
+        SPAN2.classList.toggle('-rotate-45')
+    })
+}
