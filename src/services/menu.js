8cbc82c25a1559a7de095aadfd90f8495969cf2f
linuxmobile <bdiez19@gmail.com>
2023-01-15 19:10:18 -0300

    Style 🌈: fixing style of menu

--- src/services/menu.js
+++ src/services/menu.js
@@ -2,6 +2,7 @@ const MENUTOGGLE = document.getElementById('menutoggle')
 const MENUSECTION = document.getElementById('menuSection')
 const SPAN1 = document.getElementById('span1')
 const SPAN2 = document.getElementById('span2')
+const LOGO = document.getElementById('logo')
 
 export const menuHandler = () => {
     MENUTOGGLE.addEventListener('click', () => {
@@ -12,5 +13,6 @@ export const menuHandler = () => {
         SPAN2.classList.toggle('w-4')
         SPAN2.classList.toggle('-translate-y-1.5')
         SPAN2.classList.toggle('-rotate-45')
+        LOGO.classList.toggle('text-slate-900')
     })
 }
