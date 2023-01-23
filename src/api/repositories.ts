fcd37eb2c331c0f4839eb98e216353c33d434862
linuxmobile <bdiez19@gmail.com>
2023-01-23 15:32:49 -0300

    Fix 🐞: few changes in reposections

--- src/api/repositories.ts
+++ src/api/repositories.ts
@@ -1,6 +1,6 @@
 export const getRepositories = async (): Promise<any> => {
     const data = await fetch(
-      "https://api.github.com/users/linuxmobile/repos?per_page=50",
+      "https://api.github.com/users/linuxmobile/repos?per_page=50&sort=created",
       {
         headers: {
           Authorization: `token ${import.meta.env.GITHUB_TOKEN}`,
