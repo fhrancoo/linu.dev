99263b7b33ee05bf72a21073655de812a4b7b9e6
linuxmobile <bdiez19@gmail.com>
2023-03-28 22:09:20 -0300

    Fix 🐞: fixing somethings

--- src/api/repositories.ts
+++ src/api/repositories.ts
@@ -1,13 +1,12 @@
 export const getRepositories = async (): Promise<any> => {
-    const data = await fetch(
-      "https://api.github.com/users/linuxmobile/repos?per_page=50&sort=created",
-      {
-        headers: {
-          Authorization: `token ${import.meta.env.GITHUB_TOKEN}`,
-        },
-      }
-    ).then((res) => res.json());
-  
-    return data;
-  };
-  
+	const data = await fetch(
+		'https://api.github.com/users/linuxmobile/repos?per_page=50&sort=created',
+		{
+			headers: {
+				Authorization: `token ${import.meta.env.GITHUB_TOKEN}`,
+			},
+		},
+	).then((res) => res.json())
+
+	return data
+}
