c99ac080b22d4dc4bdd89e093344b291dd2e93a4
linuxmobile <bdiez19@gmail.com>
2023-04-11 17:38:09 -0300

    Test 🧪: testing og generator

---
+++ src/services/config.ts
@@ -0,0 +1,10 @@
+export const SITE = {
+	website: 'https://linu.dev',
+	author: 'Braian A. Diez / linuxmobile',
+	desc:
+		'My personal blog and site containing information about development and other personal interests.',
+	title: 'LinuDev',
+	lightAndDarkMode: false,
+	postPerPage: 18,
+	ogImage: '',
+}
