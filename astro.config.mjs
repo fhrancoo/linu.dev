5a7eab64e9aa997669dfd392cbb42499b8ab31de
linuxmobile <bdiez19@gmail.com>
2023-04-12 19:30:19 -0300

    Fix 🐞: fixing url from twitter share btn

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://linu.dev/',
 	integrations: [tailwind(), mdx()],
 	markdown: {
 		shikiConfig: {
