60e7a63b9fa9e7e4e976690bdb77faa0cb5001e8
linuxmobile <bdiez19@gmail.com>
2023-04-13 01:05:52 -0300

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,16 @@ import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://linu.dev/',
+	site: 'https://linu.dev/',
 	integrations: [tailwind(), mdx()],
 	markdown: {
 		shikiConfig: {
-		  theme: 'dracula',
-		  // Agrega lenguajes de programación personalizados
-		  // Nota: Shiki tiene innumerables lenguajes de programación incorporados, ¡incluido .astro!
-		  // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-		  langs: [],
-		  wrap: true,
+			theme: 'dracula',
+			// Agrega lenguajes de programación personalizados
+			// Nota: Shiki tiene innumerables lenguajes de programación incorporados, ¡incluido .astro!
+			// https://github.com/shikijs/shiki/blob/main/docs/languages.md
+			langs: [],
+			wrap: true,
 		},
-	  },
-	
+	},
 })
