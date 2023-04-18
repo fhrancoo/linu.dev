1575eb0b6ab471b359f3ffb28b3a15e2dad4b60f
linuxmobile <bdiez19@gmail.com>
2023-04-18 16:53:19 -0300

    :heavy_plus_sign: Adding sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
 import fs from 'node:fs'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://linu.dev',
-	integrations: [tailwind(), mdx()],
+	integrations: [tailwind(), mdx(), sitemap()],
 	markdown: {
 		shikiConfig: {
 			theme: 'dracula',
