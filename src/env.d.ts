fd04d85ceec22424685256220227e096e820b245
linuxmobile <bdiez19@gmail.com>
2023-06-26 17:50:52 -0300

    Removing old stuff

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,10 +2,6 @@
 /// <reference types="astro/client" />
 interface ImportMetaEnv {
     readonly GITHUB_TOKEN: string;
-    readonly PUBLIC_SPOTIFY_CLIENT_ID: string;
-    readonly PUBLIC_SPOTIFY_CLIENT_SECRET: string;
-    readonly PUBLIC_SPOTIFY_REFRESH_TOKEN: string;
-    readonly PUBLIC_SPOTIFY_CODE: string;
     readonly PUBLIC_ENV: string;
   }
   
